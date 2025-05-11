f2198be15d3699f04f872afa27e51c4019c015d4
Tomasz Stolarczyk <01188982@pw.edu.pl>
2025-05-11 15:07:07 +0200

    zgoda_na_cookies_single_browser update

--- tests/zgoda_na_cookies_single_browser.py
+++ tests/zgoda_na_cookies_single_browser.py
@@ -1,33 +1,36 @@
 from playwright.sync_api import sync_playwright
 
-# Wybierz ciasteczko, po którym wolisz weryfikować obecność
+### ciasteczka, po których bedziemy weryfikować ###
 EXPECTED = {"cookiePolicyGDPR", "cookiePolicyGDPR__details", "cookiePolicyINCPS"}
 
 def test_accept_analytics_cookie_simple():
 
     with sync_playwright() as p:
+        ### uruchamiamy wybrana przegladarke headless ###
         browser = p.chromium.launch(headless=True)
         context = browser.new_context()
         page = context.new_page()
+        ### wchodzimy na ing ###
         page.goto("https://www.ing.pl", timeout=60000)
 
-        ### otwieramy panel cookies
+        ### otwieramy panel cookies ###
         page.click("button.js-cookie-policy-main-settings-button")
 
-        ### Zaznaczamy analityczne
+        ### Akceptujemy analityczne ###
         page.click("div.js-checkbox[name='CpmAnalyticalOption']")
 
-        ### Zaakceptuj zaznaczone
+        ### Akceptujemy zaznaczone ###
         page.click("button:has-text('Zaakceptuj zaznaczone')")
 
-        ###. Pobierz cookies
+        ### Pobieramy cookies ###
         cookies = context.cookies()
         names = {c["name"] for c in cookies}
 
-        ###. Asercja: co najmniej jedno z oczekiwanych ciastek jest w zestawie
+        ### Asercja: co najmniej jedno z oczekiwanych ciastek jest w zestawie ###
         assert EXPECTED & names, (
             f"Nie znaleziono żadnego z expected cookies {EXPECTED!r}, "
             f"a mamy tylko {names!r}"
         )
 
+        ### zamykamy sesje ###
         browser.close()
